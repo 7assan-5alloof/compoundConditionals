f36e017348e545a342fb1bca16517cd54c9ee816
7assan-5alloof <hassankhalloof20@gmail.com>
2021-02-08 13:32:35 +0400

    Finish forth conditional

--- activity5.7.py
+++ activity5.7.py
@@ -4,4 +4,5 @@ volumeofacylinder = 3016
 
 # print((volumeofacylinder > 4010) or (lengthofarectangle < 2048))
 # print(not((lengthofarectangle > 4) and (areaofacircle < 20)))
-print((volumeofacylinder < 4) and (areaofacircle < 2048))
+# print((volumeofacylinder < 4) and (areaofacircle < 2048))
+print(not(areaofacircle != 7.07))

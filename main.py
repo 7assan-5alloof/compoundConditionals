9ea72d284386ad6a2832990544a488abffaf0778
7assan-5alloof <hassankhalloof20@gmail.com>
2021-02-08 12:26:55 +0400

    Finish fifth conditional

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ num3 = num1 + num2
 # print((num1 > 4) or (num2 < 2048))
 # print(not((num1 > 4) and (num2 < 2048)))
 # print((num1 < 4) and (num2 < 2048))
-print(not(num3 != 3))
+# print(not(num3 != 3))
+print((num1 >= 12) and (num3 <= 9))
